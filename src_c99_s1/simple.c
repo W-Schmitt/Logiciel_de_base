d838dd2aaf2615d11eaac7da997d41a4e127a2e6
William Schmitt <william@williamschmitt.org>
2018-05-02 17:36:35 +0200

    fix: simple.c

--- src_c99_s1/simple.c
+++ src_c99_s1/simple.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <inttypes.h>
 
-#define X 5;
+#define X 5
 
 #define PLUS(a, b) a + b
 
@@ -12,23 +12,24 @@ int main(void)
     uint32_t x2 = X * X;
     printf("x = %" PRIu32 "\n", x2);
 
-    printf("9 * 10 = %" PRIu32 "\n", PLUS(5, 4) * 10);
+    printf("9 * 10 = %" PRIu32 "\n", (PLUS(5, 4)) * 10);
 
-    for (uint8_t i = 10; i >= 0; i--) {
+    for (int8_t i = 10; i >= 0; i--) {
         printf("i = %" PRIu8 "\n", i);
     }
 
     uint8_t i = 0;
-    while (i < 10)
+    while (i < 10) {
         printf("i = ");
         printf("%" PRIu8 "\n", i);
         i++;
-
+    }
     char *chaine1 = "abc";
     // n'oublions pas l'octet pour stocker le '\0' final
     char *chaine2 = malloc(strlen(chaine1) + 1 + 1);
     // on concatene le caractere 'd' a la fin de la chaine
-    chaine2 = chaine1 + 'd';
+    strcat(chaine2, chaine1);
+    strcat(chaine2, "d\0");
     printf("%s + 'd' = %s\n", chaine1, chaine2);
     free(chaine2);
 
