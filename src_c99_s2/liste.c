27d5c0dd0f698998bc6fea263487f5d2914f2f4e
William Schmitt <william@williamschmitt.org>
2018-05-02 17:37:05 +0200

    new: liste & tests

--- src_c99_s2/liste.c
+++ src_c99_s2/liste.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <inttypes.h>
+#include <stdlib.h>
+#include <assert.h>
 
 struct cell_t {
     uint8_t val;
@@ -11,46 +13,77 @@ struct liste_t {
     struct cell_t tete;
 };
 
+/**
+ * Initialise une liste chaînée.
+ */
 struct liste_t *nouv_liste(void)
 {
-    // a changer
-    return NULL;
+    struct liste_t *l = malloc(sizeof(struct liste_t));
+    assert(l != NULL);
+    l->tete.suiv = NULL;
+    return l;
 }
 
+/**
+ *
+ */
 bool est_vide_liste(struct liste_t *liste)
 {
-    // a changer
-    // le compilateur genere un avertissement si on n'utilise
-    //   pas un parametre ou une variable declare
-    // pour eviter ca, il suffit de le transtyper vers void
-    (void)liste;
-    return false;
+    return liste->tete.suiv == NULL;
 }
 
+/**
+ * Insère un élément en tête de liste
+ */
 void inserer_tete_liste(uint8_t val, struct liste_t *liste)
 {
-    // a completer
-    (void)val;
-    (void)liste;
+    struct cell_t *tmp = malloc(sizeof(*tmp));
+    assert(tmp != NULL);
+    tmp->val = val;
+    tmp->suiv = liste->tete.suiv;
+    liste->tete.suiv = tmp;
 }
 
 bool supprimer_val_liste(uint8_t val, struct liste_t *liste)
 {
-    // a changer
-    (void)val;
-    (void)liste;
+    struct cell_t *current;
+    struct cell_t *previous;
+    current = &(liste->tete);
+    previous = current;
+    do {
+        if (current->val == val) {
+            previous->suiv = current->suiv;
+            free(current);
+            return true;
+        }
+        previous = current;
+        current = current->suiv;
+    } while (current->val);
     return false;
 }
 
 void afficher_liste(struct liste_t *liste)
 {
-    // a completer
-    (void)liste;
+    uint32_t i = 0;
+    struct cell_t current = liste->tete;
+    do {
+        printf("|%" PRId32 "\t|\t%" PRId8 "|\n", i, current.val);
+        current = *current.suiv;
+        i++;
+    } while (current.val);
 }
 
 void detruire_liste(struct liste_t **liste)
 {
-    // a completer
-    (void)liste;
+    struct cell_t *current = &((*liste)->tete);
+    struct cell_t *next = current->suiv;
+    while (next != NULL) {
+        free(current);
+        current = next;
+        next = current->suiv;
+    }
+    free(next);
+    free(*liste);
+    *liste = NULL;
 }
 
--- src_c99_s2/testliste.c
+++ src_c99_s2/testliste.c
@@ -3,6 +3,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+uint8_t cnt = 0;
+uint8_t passed = 0;
+
+char* test(bool a) {
+    if (a) {
+        passed++;
+    }
+    cnt++;
+    return a ? "true" : "false";
+}
+
+void report(void) {
+    printf("\n==================\n");
+    printf("Report\n");
+    printf("==================\n");
+    printf("Passed: \t%" PRIu8 "\n", passed);
+    printf("Failed: \t%" PRIu8 "\n", cnt-passed);
+    printf("Total:  \t%" PRIu8 "\n", cnt);
+    printf("==================\n");
+}
+
 int main(void)
 {
     // la fonction srandom initialise le generateur de
@@ -12,7 +33,31 @@ int main(void)
     //   aleatoire entre 0 et une valeur max tres grande
     // si on veut par exemple recuperer des entiers entre 0 et
     //   9, on utilisera un modulo : random() % 10
-    // a completer
+
+    printf("nouv_liste crée une liste vide.\n");
+    struct liste_t *l = nouv_liste();
+    printf("\t -> %s\n", test(est_vide_liste(l)));
+
+
+    printf("inserer_tete_liste insère des éléments dans la liste.\n");
+    uint8_t val;
+    for (uint8_t i = 0; i < 10; i++) {
+        val = random() % 10;
+        inserer_tete_liste(val, l);
+    }
+    printf("\t -> %s\n", test(!est_vide_liste(l)));
+
+    afficher_liste(l);
+
+    struct liste_t *l2;
+    l2 = nouv_liste();
+    printf("detruire_liste supprime la liste.\n");
+    detruire_liste(&l2);
+    printf("\t -> %s\n", test(l2 == NULL));
+
+
+
+    report();
     return 0;
 }
 
