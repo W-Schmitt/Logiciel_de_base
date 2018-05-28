f698ed7188c1e45e4a8f4940794bd9fd0e3f6bc7
William Schmitt <william@williamschmitt.org>
2018-05-28 16:22:27 +0200

    docs: temps

--- src_de_base/interrupts.c
+++ src_de_base/interrupts.c
@@ -1,9 +1,5 @@
 #include "interrupts.h"
 
-/**
- * Initialise un traitant en encodant son adresse dans la table des
- * interruptions
- */
 void init_traitant(void (*traitant)(void), uint8_t n_interrupt)
 {
   uint32_t *ad = (uint32_t*)(INTRPT_VECT_ADD + 2 * 4 * n_interrupt);
@@ -11,9 +7,6 @@ void init_traitant(void (*traitant)(void), uint8_t n_interrupt)
   *(ad + 1) = INTRPT_CST_LOWER | ((uint32_t)(traitant) & 0xFFFF0000);
 }
 
-/**
- * Démasque une IRQ
- */
 void demasque(uint32_t n_irq)
 {
   uint8_t o;
--- src_de_base/temps.h
+++ src_de_base/temps.h
@@ -4,6 +4,17 @@
 #include "segment.h"
 #include "interrupts.h"
 
+/**
+ * Traite une interruption de la clock
+ */
 void tic_PIT(void);
+
+/**
+ * Définit la valeur de la fréquence de la clock
+ */
 void set_freq(void);
+
+/**
+ * Déclaration pour la fonction assembleur correspondante
+ */
 void traitant_IT_32();
